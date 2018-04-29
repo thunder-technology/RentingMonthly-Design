1ef59e6d36efd8cf4738677bee3e5313b4cedf31
Haolin Dai <harry442930583@gmail.com>
2018-04-29 08:34:08 +0800

    php files

--- select.php
+++ select.php
@@ -2,22 +2,26 @@
 	$servername = "localhost";
 	$username = "root";
 	$pass = "rentingmonthly";
+	$db = "estate";
 
-	$con = new mysqli($servername, $username, $pass);
+	$con = new mysqli($servername, $username, $pass, $db);
 
 	if ($con->connect_error)
 		die("Connection failed" . $con->connect_error);
 	
 	$sql = "SELECT * FROM estate WHERE status = FALSE";
-	$result = $con->query($sql);
-
+	$result = $con->query($sql);  // apply query 
+	
 	if ($result-> num_rows > 0)
-		while ($rows = $result->fetch_assoc())
+		while ($rows = $result->fetch_assoc())	// parsing data from query
+		{
 			echo "<br> Type: " . $rows["type" ] .
-				 "Start Time: " . $rows["start_time"] .
+			 "Start Time: " . $rows["start_time"] .
 				 "End Time: " . $rows["end_time"];
+		}
 	else
 		echo "0 results";
+	
 	
 	$con->close();
 ?>
