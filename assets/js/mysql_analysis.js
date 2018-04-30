2a25037e06235da74306c66649655bbaee8d3f63
Haolin Dai <harry442930583@gmail.com>
2018-04-30 15:20:20 +0800

    testing mysql

--- assets/js/mysql_analysis.js
+++ assets/js/mysql_analysis.js
@@ -1,5 +1,5 @@
 $.get("select.php", function (data) {
-        $(".north_york")
+        $("#north_york")
             .append(" " + data.estate_type)
             .append(" " + data.start_time)
             .append(" " + data.end_time);
--- select.php
+++ select.php
@@ -9,14 +9,18 @@
 	if ($con->connect_error)
 		die("Connection failed" . $con->connect_error);
 
-	$sql = "SELECT * FROM estate WHERE status = FALSE";
+	$sql = "SELECT * FROM estate";
 	$result = $con->query($sql);  // apply query
 
 	if ($result-> num_rows > 0)
 	{
 		while ($rows = $result->fetch_assoc())	// parsing data from query
 		{
-			echo json_encode(array("estate_type" => $rows["type"], "start_time" => $rows["start_time", "end_time" => $rows["end_time"]));
+			echo json_encode(array("estate_type" => $rows["type"],
+			                       "start_time" => $rows["start_time"],
+			                       "end_time" => $rows["end_time"],
+			                       "status" => $rows["status"]
+			                       ));
 		}
 	}
 	else
