63366e0719d814bd68ba32ef5388387d27db8225
Haolin Dai <harry442930583@gmail.com>
2018-05-01 09:36:19 +0800

    testing mysql

--- assets/js/mysql_analysis.js
+++ assets/js/mysql_analysis.js
@@ -1,5 +1,6 @@
 $.get("select.php", function (data) {
         $(".north_york")
+            .alert("Successfully implemented!")
             .append("<li>" + data.estate_type)
             .append(" " + data.start_time)
             .append(" " + data.end_time + "</li>");
