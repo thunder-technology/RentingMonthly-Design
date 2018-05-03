e3eff38c584b7048c1b1f351c04633aea27d3cf6
Haolin Dai <harry442930583@gmail.com>
2018-05-03 14:59:13 +0800

    index.html数据可视化(north york部分)

--- assets/js/select.js
+++ assets/js/select.js
@@ -17,9 +17,39 @@ $.get("select.php", function (data) {
                     .append("</li>")
                     .append('</div>');
             }
-            else if (status === "0")
+            else if ($(value).attr("status") === "0")
             {
                 $("#north_york_unsold")
+                    .append("<li>")
+                    .append('<div class="cosB">')
+                    .append("从" + $(value).attr("start_time") + "到" + $(value).attr("end_time"))
+                    .append('</div>')
+                    .append('<div> class="cosA"')
+                    .append('<span>')
+                    .append("房屋类型：" + $(value).find("type").text())
+                    .append('</span>')
+                    .append("</li>")
+                    .append('</div>');
+            }
+        }
+        else if ($(value).attr("area") === "MISSISAUGA") {
+            if ($(value).attr("status") === "1")
+            {
+                $("#missisauga_sold")
+                    .append("<li>")
+                    .append('<div class="cosB">')
+                    .append("从" + $(value).attr("start_time") + "到" + $(value).attr("end_time"))
+                    .append('</div>')
+                    .append('<div> class="cosA"')
+                    .append('<span>')
+                    .append("房屋类型：" + $(value).find("type").text())
+                    .append('</span>')
+                    .append("</li>")
+                    .append('</div>');
+            }
+            else if ($(value).attr("status") === "0")
+            {
+                $("#missisauge_unsold")
                     .append("<li>")
                     .append('<div class="cosB">')
                     .append("从" + $(value).attr("start_time") + "到" + $(value).attr("end_time"))
