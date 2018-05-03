c7585876421abde7b6ad381e59de8f66d8f03ac3
Haolin Dai <harry442930583@gmail.com>
2018-05-03 14:32:11 +0800

    index.html数据可视化(north york部分)

--- assets/js/select.js
+++ assets/js/select.js
@@ -1,17 +1,34 @@
 $.get("select.php", function (data) {
-    var markers = $(data).find('marker');
-    var area = markers.attr("area");
-    if (area === "NORTH YORK") {
-        $("#north_york_sold")
-            .append("<li>")
-            .append('<div class="cosB">')
-            .append("从" + markers.attr("start_time") + "到" + markers.attr("end_time"))
-            .append('</div>')
-            .append('<div> class="cosA"')
-            .append('<span>')
-            .append("房屋类型：" + markers.find("type").text())
-            .append('</span>')
-            .append("</li>")
-            .append('</div>');
-    }
+    $(data).find("marker").eq(1).each(function(){
+        if (this.attr("area") === "NORTH YORK") {
+            if (this.attr("status") === "1")
+            {
+                $("#north_york_sold")
+                    .append("<li>")
+                    .append('<div class="cosB">')
+                    .append("从" + this.attr("start_time") + "到" + this.attr("end_time"))
+                    .append('</div>')
+                    .append('<div> class="cosA"')
+                    .append('<span>')
+                    .append("房屋类型：" + markers.find("type").text())
+                    .append('</span>')
+                    .append("</li>")
+                    .append('</div>');
+            }
+            else if (status === "0")
+            {
+                $("#north_york_unsold")
+                    .append("<li>")
+                    .append('<div class="cosB">')
+                    .append("从" + this.attr("start_time") + "到" + this.attr("end_time"))
+                    .append('</div>')
+                    .append('<div> class="cosA"')
+                    .append('<span>')
+                    .append("房屋类型：" + this.find("type").text())
+                    .append('</span>')
+                    .append("</li>")
+                    .append('</div>');
+            }
+        }
+    });
 });
