ec0eaf2f6095de993523073f660180f984abff11
Haolin Dai <harry442930583@gmail.com>
2018-05-03 13:40:05 +0800

    新增select.js提取XML数据并输出

--- assets/js/select.js
+++ assets/js/select.js
@@ -2,17 +2,16 @@ $.get("select.php", function (data) {
     var markers = $(data).find('marker');
     var area = markers.attr("area");
     console.log(area);
+    if (area === "NORTH YORK") {
+        $("#north_york_sold")
+            .append("<li>")
+            .append('<div class="cosB">')
+            .append("from" + markers.find("start_time").text() + "to" + markers.find("end_time").text())
+            .append('</div>')
+            .append('<div> class="cosA"')
+            .append('<span>')
+            .append("房屋类型：" + markers.find("type").text())
+            .append('</span>')
+            .append("</li>");
+    }
 });
-
-if (area === "NORTH YORK") {
-    $("#north_york_sold")
-        .append("<li>")
-        .append('<div class="cosB">')
-        .append("from" + markers.find("start_time").text() + "to" + markers.find("end_time").text())
-        .append('</div>')
-        .append('<div> class="cosA"')
-        .append('<span>')
-        .append("房屋类型：" + markers.find("type").text())
-        .append('</span>')
-        .append("</li>");
-}
