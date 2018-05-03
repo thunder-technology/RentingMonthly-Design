a2301929c089cb7a6affc86494bd2a9658227da4
Haolin Dai <harry442930583@gmail.com>
2018-05-03 15:54:08 +0800

    升级优化插入内容

--- assets/js/select.js
+++ assets/js/select.js
@@ -2,33 +2,33 @@
 $.get("select.php", function (data) {
     // Iteratively go through each marker tag
     $(data).find("marker").each(function(index, value) {
-        var $content = $("")
-            .append("<li>")
-            .append('<div class="cosB">')
-            .append("从" + $(value).attr("start_time") + "到" + $(value).attr("end_time"))
-            .append('</div>')
-            .append('<div class="cosA">')
-            .append('<span>')
-            .append("房屋类型：" + $(value).attr("type"))
-            .append('</span>')
-            .append('</div>')
-            .append("</li>");
-
+        var content = "";
+        content
+            .concat("<li>")
+            .concat('<div class="cosB">')
+            .concat("从" + $(value).attr("start_time") + "到" + $(value).attr("end_time"))
+            .concat('</div>')
+            .concat('<div class="cosA">')
+            .concat('<span>')
+            .concat("房屋类型：" + $(value).attr("type"))
+            .concat('</span>')
+            .concat('</div>')
+            .concat("</li>");
+        
         if ($(value).attr("area") === "NORTH YORK") {
-            alert($(value).attr("status"));
             if ($(value).attr("status") === "1") {
-                $("#north_york_sold").append($content.clone());
+                $("#north_york_sold").append(content);
             }
             else if ($(value).attr("status") === "0") {
-                $("#north_york_unsold").append($content.clone());
+                $("#north_york_unsold").append(content);
             }
         }
         else if ($(value).attr("area") === "MISSISAUGA") {
             if ($(value).attr("status") === "1") {
-                $("#missisauga_sold").append($content.clone());
+                $("#missisauga_sold").append(content);
             }
             else if ($(value).attr("status") === "0") {
-                $("#missisauge_unsold").append($content.clone());
+                $("#missisauge_unsold").append(content);
             }
         }
     });
