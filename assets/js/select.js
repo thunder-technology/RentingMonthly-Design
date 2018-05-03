36d71954cdb8204d84a5f2089f8f79035e224023
Haolin Dai <harry442930583@gmail.com>
2018-05-03 14:37:20 +0800

    index.html数据可视化(north york部分)

--- assets/js/select.js
+++ assets/js/select.js
@@ -1,16 +1,18 @@
+// retrieve the XML data from select.php
 $.get("select.php", function (data) {
-    $(data).find("marker").eq(1).each(function(){
-        if (this.attr("area") === "NORTH YORK") {
-            if (this.attr("status") === "1")
+    // Iteratively go through each marker tag
+    $(data).find("marker").eq(1).each(function() {
+        if ($(this).attr("area") === "NORTH YORK") {
+            if ($(this).attr("status") === "1")
             {
                 $("#north_york_sold")
                     .append("<li>")
                     .append('<div class="cosB">')
-                    .append("从" + this.attr("start_time") + "到" + this.attr("end_time"))
+                    .append("从" + $(this).attr("start_time") + "到" + $(this).attr("end_time"))
                     .append('</div>')
                     .append('<div> class="cosA"')
                     .append('<span>')
-                    .append("房屋类型：" + markers.find("type").text())
+                    .append("房屋类型：" + $(this).find("type").text())
                     .append('</span>')
                     .append("</li>")
                     .append('</div>');
@@ -20,11 +22,11 @@ $.get("select.php", function (data) {
                 $("#north_york_unsold")
                     .append("<li>")
                     .append('<div class="cosB">')
-                    .append("从" + this.attr("start_time") + "到" + this.attr("end_time"))
+                    .append("从" + $(this).attr("start_time") + "到" + $(this).attr("end_time"))
                     .append('</div>')
                     .append('<div> class="cosA"')
                     .append('<span>')
-                    .append("房屋类型：" + this.find("type").text())
+                    .append("房屋类型：" + $(this).find("type").text())
                     .append('</span>')
                     .append("</li>")
                     .append('</div>');
