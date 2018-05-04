5fb8c8b9d9a95c7ef207eab376d92045886cb306
Haolin Dai <harry442930583@gmail.com>
2018-05-04 22:05:15 +0800

    谷歌地图数据对接测试

--- assets/js/map.js
+++ assets/js/map.js
@@ -12,22 +12,6 @@ $.get("select.php", function (data) {
     });
 });
 
-var map;
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 12,
-        center: new google.maps.LatLng(43.6532, -79.3832),
-        mapTypeId: 'terrain'
-    });
-
-    // Create a <script> tag and set the USGS URL as the source.
-    var script = document.createElement('script');
-    // This example uses a local copy of the GeoJSON stored at
-    // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-    script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-    document.getElementsByTagName('head')[0].appendChild(script);
-}
-
 // Loop through the results array and place a marker for each
 // set of coordinates.
 window.eqfeed_callback = function(results) {
