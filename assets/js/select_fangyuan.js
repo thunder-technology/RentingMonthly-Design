5d6e2779c1b0cc243b30b0e22896e439c11022af
Haolin Dai <harry442930583@gmail.com>
2018-05-04 17:08:58 +0800

    房源信息对接

---
+++ assets/js/select_fangyuan.js
@@ -0,0 +1,31 @@
+$.get("select.php", function (data) {
+    //添加内容到房源信息页面
+    $(data).find("marker").each(function(index, value) {
+        var fangyuan = ""
+            .concat("<tr>")
+                .concat("<td>" + "<input type=checkbox>" + "</td>")
+                .concat("<td>" + $(value).attr("id") + "</td>")
+                .concat("<td>" + '<a href="">' + $(value).attr("address") + "</a>" + "</td>")
+                .concat("<td>" + $(value).attr("estate_type") + "</td>")
+                .concat('<td class="am-hide-sm-only">' + zicarb +"</td>")
+                .concat('<td class="am-hide-sm-only">' +  Date.now().toString() + "</td>")
+            .concat("</tr>")
+            .concat('<td class="am-hide-sm-only">')
+                .concat('<div class="am-btn-toolbar">')
+                    .concat('<div class="am-btn-group am-btn-group-xs">')
+                        //这个按钮是出于方便后台管理，通过这个按钮可以让这项数据在前台的时候不被显示
+                        .concat('<button class="am-btn am-btn-default am-btn-xs am-text-secondary"><span class="am-icon-pencil-square-o"></span> 编辑</button>')
+                        .concat('<button class="am-btn am-btn-default am-btn-xs am-hide-sm-only"><span class="am-icon-chain"></span>下架</button>')
+                        //这个按钮是让这项数据在前台可以显示
+                        .concat('<button class="am-btn am-btn-default am-btn-xs am-hide-sm-only"><span class="am-icon-chain-broken"></span>下架</button>')
+                        .concat('<button class="am-btn am-btn-default am-btn-xs am-text-danger am-hide-sm-only"><span class="am-icon-trash-o"></span> 删除</button>')
+                    .concat('</div>')
+                .concat('</div>')
+            .concat('</td>');
+
+        alert(fangyuan);
+        //添加到列表
+        $("#fangyuan")
+            .append(fangyuan);
+    });
+});
---
+++ assets/js/select_index.js
@@ -0,0 +1,34 @@
+// retrieve the XML data from select.php
+$.get("select.php", function (data) {
+    // Iteratively go through each marker tag
+    $(data).find("marker").each(function(index, value) {
+
+        var content = ""
+            .concat("<li>")
+            .concat('<div class="cosA">')
+            .concat('<span>')
+            .concat("房屋类型：" + $(value).attr("estate_type") + "房屋地址: " + $(value).attr("address") + "<br>")
+            .concat($(value).attr("start_time") + "至" + $(value).attr("end_time"))
+            .concat('</span>')
+            .concat('</div>')
+            .concat("</li>");
+
+        // 添加内容到主页
+        if ($(value).attr("area") === "NORTH YORK") {
+            if ($(value).attr("status") === "1") {
+                $("#north_york_sold").append(content);
+            }
+            else if ($(value).attr("status") === "0") {
+                $("#north_york_unsold").append(content);
+            }
+        }
+        else if ($(value).attr("area") === "MISSISAUGA") {
+            if ($(value).attr("status") === "1") {
+                $("#missisauga_sold").append(content);
+            }
+            else if ($(value).attr("status") === "0") {
+                $("#missisauge_unsold").append(content);
+            }
+        }
+    });
+});
