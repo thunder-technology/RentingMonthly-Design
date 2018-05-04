6b737a0cd3cfe732c6c38578ae74ebc45d0d2721
Haolin Dai <harry442930583@gmail.com>
2018-05-04 21:43:15 +0800

    房源信息对接

--- assets/js/select_fangyuan.js
+++ assets/js/select_fangyuan.js
@@ -9,21 +9,20 @@ $.get("select.php", function (data) {
                 .concat("<td>" + $(value).attr("estate_type") + "</td>")
                 .concat('<td class="am-hide-sm-only">zicarb</td>')
                 .concat('<td class="am-hide-sm-only">' +  Date.now().toString() + "</td>")
-            .concat("</tr>")
-            .concat('<td class="am-hide-sm-only">')
-                .concat('<div class="am-btn-toolbar">')
-                    .concat('<div class="am-btn-group am-btn-group-xs">')
-                        //这个按钮是出于方便后台管理，通过这个按钮可以让这项数据在前台的时候不被显示
-                        .concat('<button class="am-btn am-btn-default am-btn-xs am-text-secondary"><span class="am-icon-pencil-square-o"></span> 编辑</button>')
-                        .concat('<button class="am-btn am-btn-default am-btn-xs am-hide-sm-only"><span class="am-icon-chain"></span>下架</button>')
-                        //这个按钮是让这项数据在前台可以显示
-                        .concat('<button class="am-btn am-btn-default am-btn-xs am-hide-sm-only"><span class="am-icon-chain-broken"></span>下架</button>')
-                        .concat('<button class="am-btn am-btn-default am-btn-xs am-text-danger am-hide-sm-only"><span class="am-icon-trash-o"></span> 删除</button>')
+                .concat('<td class="am-hide-sm-only">')
+                    .concat('<div class="am-btn-toolbar">')
+                        .concat('<div class="am-btn-group am-btn-group-xs">')
+                            //这个按钮是出于方便后台管理，通过这个按钮可以让这项数据在前台的时候不被显示
+                            .concat('<button class="am-btn am-btn-default am-btn-xs am-text-secondary"><span class="am-icon-pencil-square-o"></span> 编辑</button>')
+                            .concat('<button class="am-btn am-btn-default am-btn-xs am-hide-sm-only"><span class="am-icon-chain"></span>下架</button>')
+                            //这个按钮是让这项数据在前台可以显示
+                            .concat('<button class="am-btn am-btn-default am-btn-xs am-hide-sm-only"><span class="am-icon-chain-broken"></span>下架</button>')
+                            .concat('<button class="am-btn am-btn-default am-btn-xs am-text-danger am-hide-sm-only"><span class="am-icon-trash-o"></span> 删除</button>')
+                        .concat('</div>')
                     .concat('</div>')
-                .concat('</div>')
-            .concat('</td>');
+                .concat('</td>')
+            .concat("</tr>");
 
-        alert(fangyuan);
         //添加到列表
         $("#fangyuan")
             .append(fangyuan);
