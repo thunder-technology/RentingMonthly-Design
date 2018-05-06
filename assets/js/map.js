dae2e9b0c5d8a7f43226971a4ea628d9cb7b29c2
Haolin Dai <harry442930583@gmail.com>
2018-05-07 03:41:53 +0800

    谷歌地图数据对接测试

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,26 +1,19 @@
 
-var addresses = [];
-
-function initialize()
-{
-    initMap().done(geocode());
-}
+var map; // global variable
 
 function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 12,
-            center: new google.maps.LatLng(43.6532, -79.3832),
-            mapTypeId: 'terrain'
-        });
-    marker.addListener('click', function (marker) {
-        if (marker.getAnimation() !== null) {
-            marker.setAnimation(null);
-        } else {
-            marker.setAnimation(google.maps.Animation.BOUNCE);
-        }
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 12,
+        center: new google.maps.LatLng(43.6532, -79.3832),
+        mapTypeId: 'terrain'
     });
 
-    var infoWindow = new google.maps.InfoWindow;
+    google.maps.event.addListener( map, 'idle', function() {
+        geocode();
+    });
+}
+
+function geocode() {
     //获取数据库的所有租房信息
     $.get('select.php', function(data) {
         // 分析xml数据
@@ -30,36 +23,42 @@ function initMap() {
             // 提取地址数据
             var address = $(markerElem).attr('address');
             // 地址 -> 坐标
-            addresses.push(address);
+            axios.get("https://maps.googleapis.com/maps/api/geocode/json", {
+                params: {
+                    address : addresses[i],
+                    key : 'AIzaSyDao1DC4cHMICPAzOH93K4nZaswFnk4wP4'
+                }
+            })
+            // 异步编程，数据通过ajax添加到map
+                .then(
+                    function (response) {
+                        // 提取坐标数据
+                        var location = response.data.results[0].geometry.location;
+                        console.log(location);
+                        var infoWindow = new google.maps.InfoWindow;
+                        var marker = new google.maps.Marker({
+                            map: map,
+                            position: location,
+                            draggable: true,
+                            animation: google.maps.Animation.DROP
+                        });
+                        marker.addListener('click', function () {
+                            if (marker.getAnimation() !== null) {
+                                marker.setAnimation(null);
+                            } else {
+                                marker.setAnimation(google.maps.Animation.BOUNCE);
+                            }
+                        });
+                    }, function (error) {
+                        console.log(error);
+                    })
         });
     });
 }
 
 
 
-function geocode() {
-    for(var i = 0; i < addresses.length; i++) {
-        axios.get("https://maps.googleapis.com/maps/api/geocode/json", {
-            params: {
-                address : addresses[i],
-                key : 'AIzaSyDao1DC4cHMICPAzOH93K4nZaswFnk4wP4'
-            }
-        })
-        // 异步编程，数据通过ajax添加到map
-            .then(
-                function (response) {
-                    // 提取坐标数据
-                    var location = response.data.results[0].geometry.location;
-                    console.log(location);
-                    var marker = new google.maps.Marker({
-                        map: map,
-                        position: location,
-                        draggable: true,
-                        animation: google.maps.Animation.DROP
-                    });
-                }, function (error) {
-                    console.log(error);
-                })
-    }
-}
+
+
+
 
