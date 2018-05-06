008c6737f7831af719a02edf283135d735bfc183
Haolin Dai <harry442930583@gmail.com>
2018-05-06 16:53:28 +0800

    谷歌地图数据对接测试

--- assets/js/map.js
+++ assets/js/map.js
@@ -10,36 +10,58 @@ function initMap() {
     $.get('select.php', function(data) {
         var markers = $(data).find('marker');
         Array.prototype.forEach.call(markers, function(markerElem) {
-            var type = $(markerElem).attr('type');
-            var pos = geocode($(markerElem).attr('address'));
-            var marker = new google.maps.Marker({
-                map: map,
-                position: pos,
-                draggable: true,
-                animation: google.maps.Animation.DROP
-            });
-            marker.addListener('click', function() {
-                window.location.href = this.url;
-            });
+            var address = geocode($(markerElem).attr('address'));
+            axios.get("https://maps.googleapis.com/maps/api/geocode/json", {
+                params: {
+                    address : address,
+                    key : 'AIzaSyDao1DC4cHMICPAzOH93K4nZaswFnk4wP4'
+                }
+            })
+                .then(
+                //     function(response) {
+                //
+                //     // Log Full response
+                //     var location = response.data.results[0].geometry.location;
+                //     console.log(location);
+                //     return {lat :location.lat, lng :location.lng};
+                // }
+                    function getPropertyMarkers(response) {
+                        var location = response.data.results[0].geometry.location;
+                        console.log(location);
+                        $.ajax({
+                            url: "Service/MapData.asmx/getPropertyMarkers",
+                            type: "POST",
+                            data: "{'response': '" + response + "'}",
+                            async: true,
+                            cache: true,
+                            contentType: "application/json;",
+                            dataType: "json",
+                            success: function (data, textStatus, jqXHR) { //
+                                var marker = new google.maps.Marker({
+                                    map: map,
+                                    position: location,
+                                    draggable: true,
+                                    animation: google.maps.Animation.DROP
+                                });
+                                marker.addListener('click', function() {
+                                    window.location.href = this.url;
+                                });
+                            },
+                            error: function (xmlHttpRequest, textStatus, errorThrown) {
+                                console.log(xmlHttpRequest.responseText);
+                                console.log(textStatus);
+                                console.log(errorThrown);
+                                alert("Screen shot this error: " + xmlHttpRequest.toString() + " " + textStatus.toString() + " " + errorThrown.toString());
+                            }
+                        });
+                    }
+                , function (error) {
+                    console.log(error);
+                });
         });
     });
 }
 
 function geocode(address) {
-    axios.get("https://maps.googleapis.com/maps/api/geocode/json", {
-        params: {
-            address : address,
-            key : 'AIzaSyDao1DC4cHMICPAzOH93K4nZaswFnk4wP4'
-        }
-    })
-        .then(function(response)
-        {
-            // Log Full response
-            var location = response.data.results[0].geometry.location;
-            console.log(location);
-            return {lat :location.lat, lng :location.lng};
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+
 }
